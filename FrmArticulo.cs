5ba6e71ddbcd1eee53694b4fbd88d2a8b8afa77e
KaLuKaLuSoft <diego_2004_13@hotmail.com>
2021-09-01 10:05:32 -0400

    Agregar archivos de proyecto.

---
+++ FrmArticulo.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Capa_Negocio;
+
+namespace Proyecto
+{
+    public partial class FrmArticulo : Form
+    {
+        private bool IsNuevo = false;
+        private bool IsEditar = false;
+        private static FrmArticulo _Instancia;
+        public static FrmArticulo GetInstancia()
+        {
+            if (_Instancia == null)
+            {
+                _Instancia = new FrmArticulo();
+            }
+            return _Instancia;
+        }
+        public void setCategoria(string idcategoria, string nombre)
+        {
+            this.txtIdcategoria.Text = idcategoria;
+            this.txtCategoria.Text = nombre;
+        }
+        public FrmArticulo()
+        {
+            InitializeComponent();
+            this.ttMensaje.SetToolTip(this.txtNombre, "Ingrese el Nombre del Artículo");
+            this.ttMensaje.SetToolTip(this.pxImagen, "Seleccione la Imagen del Artículo");
+            this.ttMensaje.SetToolTip(this.txtCategoria, "Seleccione la Categoría del Artículo");
+            this.ttMensaje.SetToolTip(this.cbIdpresentacion, "Seleccione la presentación del Artículo");
+
+            this.txtIdcategoria.Visible = false;
+            this.txtCategoria.ReadOnly = true;
+            this.LlenarComboPresentacion();
+        }
+        //Mostrar Mensaje de Confirmación
+        private void MensajeOk(string mensaje)
+        {
+            MessageBox.Show(mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        //Mostrar Mensaje de Error
+        private void MensajeError(string mensaje)
+        {
+            MessageBox.Show(mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        //Limpiar todos los controles del formulario
+        private void Limpiar()
+        {
+            this.txtCodigo.Text = string.Empty;
+            this.txtNombre.Text = string.Empty;
+            this.txtDescripcion.Text = string.Empty;
+            this.txtIdcategoria.Text = string.Empty;
+            this.txtCategoria.Text = string.Empty;
+            this.txtIdarticulo.Text = string.Empty;
+            this.pxImagen.Image = global::Proyecto.Properties.Resources.file;
+        }
+        //Habilitar los controles del formulario
+        private void Habilitar(bool valor)
+        {
+            this.txtCodigo.ReadOnly = !valor;
+            this.txtNombre.ReadOnly = !valor;
+            this.txtDescripcion.ReadOnly = !valor;
+            this.btnBuscarCategoria.Enabled = valor;
+            this.cbIdpresentacion.Enabled = valor;
+            this.btnCargar.Enabled = valor;
+            this.btnLimpiar.Enabled = valor;
+            this.txtIdarticulo.ReadOnly = !valor;
+        }
+        //Habilitar los botones
+        private void Botones()
+        {
+            if (this.IsNuevo || this.IsEditar) //Alt + 124
+            {
+                this.Habilitar(true);
+                this.btnNuevo.Enabled = false;
+                this.btnGuardar.Enabled = true;
+                this.btnEditar.Enabled = false;
+                this.btnCancelar.Enabled = true;
+            }
+            else
+            {
+                this.Habilitar(false);
+                this.btnNuevo.Enabled = true;
+                this.btnGuardar.Enabled = false;
+                this.btnEditar.Enabled = true;
+                this.btnCancelar.Enabled = false;
+            }
+        }
+        //Método para ocultar columnas
+        private void OcultarColumnas()
+        {
+            this.dataListado.Columns[0].Visible = false;
+            this.dataListado.Columns[1].Visible = false;
+            this.dataListado.Columns[6].Visible = false;
+            this.dataListado.Columns[8].Visible = false;
+        }
+        //Método Mostrar
+        private void Mostrar()
+        {
+            this.dataListado.DataSource = NArticulo.Mostrar();
+            this.OcultarColumnas();
+            lblTotal.Text = "Total de Registros: " + Convert.ToString(dataListado.Rows.Count);
+        }
+        //Método BuscarNombre
+        private void BuscarNombre()
+        {
+            this.dataListado.DataSource = NArticulo.BuscarNombre(this.txtBuscar.Text);
+            this.OcultarColumnas();
+            lblTotal.Text = "Total de Registros: " + Convert.ToString(dataListado.Rows.Count);
+        }
+
+        private void LlenarComboPresentacion()
+        {
+            cbIdpresentacion.DataSource = NPresentacion.Mostrar();
+            cbIdpresentacion.ValueMember = "idpresentacion";
+            cbIdpresentacion.DisplayMember = "nombre";
+
+        }
+    }
+}
